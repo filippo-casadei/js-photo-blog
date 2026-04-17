ddba0dcbcaddd8d559af5e9f589dfd7f6cd01e7a
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-17 13:06:57 +0200

    Modificato file index.html: - corretto il collegamento a JS Modificato il file script.js: - creata la funzione per la chiamataAPI con fetch, then, then e catch per visualizzare i dati in console.

---
+++ script.js
@@ -0,0 +1,18 @@
+// FUNZIONE PER LA CHIAMATA API
+
+const chiamataApi = function() {
+    fetch(`https://lanciweb.github.io/demo/api/pictures/`)
+
+    .then(function(risposta) {
+        return risposta.json()
+    })
+    .then(function(dato) {
+        return console.log(dato)
+    })
+    .catch(function(error) {
+            console.log("ERRORE")
+        
+    })
+}
+
+chiamataApi();
