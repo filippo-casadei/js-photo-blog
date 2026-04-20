844c61f404fb98d62b38c2631c33f63a4019f053
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-20 15:58:53 +0200

    Modificato il file script.js: - aggiunti EventListener per i click sull'immagine e sul bottone"chiudi".

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // COLLEGAMENTO DOM
 let riga = document.querySelector(`.row`);
+let overlayDiHtml = document.querySelector(`.overlay`);
+let overlayImmagine = document.querySelector(`.overlay-img`);
 
 // FUNZIONE PER LA CHIAMATA API
 const chiamataApi = function() {
@@ -25,7 +27,7 @@ let creaPolaroidHtml = function(elemento) {
     <div class="col-12 col-md-6 col-lg-4">
         <div class="polaroid-border border bg-white p-3 pb-4">
             <img class="chiodo" src="./img/pin.svg">
-            <img class="img-fluid" src="${elemento.url}">
+            <img class="bellissima-foto img-fluid" src="${elemento.url}">
             <div class="polaroid-testo">
                 <p class="font-titolo">${elemento.title}</p>
                 <p class="font-date">${elemento.date}</p>
@@ -47,4 +49,19 @@ let creaPolaroid = function(array) {
 
 chiamataApi();
 
+// EVENT LISTENER CLICK SU POLAROID //
+riga.addEventListener("click", function(e) {
+    let polaroid = e.target.closest(`.polaroid-border`)
+    let immagineDellaPolaroid = polaroid.querySelector(`.bellissima-foto`)
+    let immagineDellaPolaroidSrc = immagineDellaPolaroid.src 
+    overlayImmagine.src = immagineDellaPolaroidSrc
+    overlayDiHtml.classList.remove(`d-none`)
+});
+
+// PRESO BTN DAL DOM + EVENT LISTENER CLICK SUL BOTTONE //
+let bottoneChiudi = document.querySelector(`.btn`)
+
+bottoneChiudi.addEventListener("click", function() {
+    overlayDiHtml.classList.add(`d-none`)
+});
 
